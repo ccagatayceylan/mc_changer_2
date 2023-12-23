bbf2b51b617cac897db8fbc536fe5ab23d0e019d
ccagatayceylan <119412495+ccagatayceylan@users.noreply.github.com>
2023-12-24 00:37:33 +0300

    Add files via upload

---
+++ mc_changer_2.py
@@ -0,0 +1,17 @@
+import subprocess
+import optparse
+
+parse_object = optparse.OptionParser()
+parse_object.add_option("-i", "--interface", dest="interface", help="interface to cahange")
+parse_object.add_option("-m", "--mac", dest="mac_address",help="new mac address" )
+
+(user_inputs, arguments) = parse_object.parse_args()
+
+user_interface = user_inputs.interface
+user_mac_adress = user_inputs.mac_address
+
+print("MyMacChanger started!")
+
+subprocess.call(["ifconfig", user_interface,"down"])
+subprocess.call(["ifconfig", user_interface, "hw", "ether", user_mac_adress])
+subprocess.call(["ifconfig", user_interface,"up"])
